3698d999e09656b236341c448788c681e3e72fe4
paulaproxy@gmail.com <paulaproxy@gmail.com>
2012-09-01 20:18:12 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+##Swapp
+
+Web app for posting and sharing info 
+about **swap events, places and community**
+(clothing, books, other goods)
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Swap::Application.config.secret_token = '86ec545dbc7e5fb6619ed17e295502870f52371000b004672625ef4ebb80f922d47fab329da1b30e4caf9fe576c636e2478734ba1ff983a19d4efe828ede8caf'
